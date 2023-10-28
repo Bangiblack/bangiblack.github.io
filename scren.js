ef3eaaf13a1221f593e94d2da3837c699b7dcb21
Bangiblack <120809181+Bangiblack@users.noreply.github.com>
2023-10-28 14:10:32 +0700

    Update scren.js

--- scren.js
+++ scren.js
@@ -1,52 +1,66 @@
-const start = document.getElementById("start");
-const stop = document.getElementById("stop");
-const download = document.getElementById("download");
-const video = document.querySelector("video");
-let recorder, stream;
+let mediaRecorder;
+let recordedChunks = [];
 
-async function startRecording() {
-  stream = await navigator.mediaDevices.getDisplayMedia({
-    // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-    audio: true,
-    video: { mediaSource: "screen", displaySurface: "monitor" }
-  });
-  recorder = new MediaRecorder(stream, {
-    // https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder
-    mimeType: "video/webm"
-  });
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const downloadButton = document.getElementById('download');
+const videoElement = document.getElementById('recording');
 
-  const chunks = [];
-  recorder.ondataavailable = (e) => chunks.push(e.data);
-  recorder.onstop = (e) => {
-    video.src = URL.createObjectURL(new Blob(chunks, { type: chunks[0].type }));
-  };
+// Disable the stop and download buttons initially
+stopButton.disabled = true;
+downloadButton.disabled = true;
 
-  recorder.start();
+startButton.addEventListener('click', startRecording);
+stopButton.addEventListener('click', stopRecording);
+downloadButton.addEventListener('click', downloadRecording);
+
+function startRecording() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  recordedChunks = [];
+
+  navigator.mediaDevices.getDisplayMedia({ video: true, audio: true })
+    .then(stream => {
+      mediaRecorder = new MediaRecorder(stream);
+
+      mediaRecorder.ondataavailable = function(event) {
+        if (event.data.size > 0) {
+          recordedChunks.push(event.data);
+        }
+      };
+
+      mediaRecorder.onstop = function() {
+        startButton.disabled = false;
+        stopButton.disabled = true;
+        downloadButton.disabled = false;
+
+        const videoBlob = new Blob(recordedChunks, { type: 'video/webm' });
+        videoElement.src = URL.createObjectURL(videoBlob);
+      };
+
+      mediaRecorder.start();
+    })
+    .catch(err => {
+      console.error('Error accessing screen:', err);
+    });
 }
 
-start.addEventListener("click", () => {
-  start.setAttribute("disabled", true);
-  stop.removeAttribute("disabled");
-  download.setAttribute("disabled", true);
+function stopRecording() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  downloadButton.disabled = false;
 
-  startRecording();
-});
+  mediaRecorder.stop();
+}
 
-stop.addEventListener("click", () => {
-  stop.setAttribute("disabled", true);
-  start.removeAttribute("disabled");
-  download.removeAttribute("disabled");
-  recorder.stop();
-  stream.getVideoTracks()[0].stop();
-});
-
-download.addEventListener("click", () => {
-  const link = document.createElement("a");
-  link.style.display = "none";
-  link.setAttribute("download", "video.webm");
-  link.setAttribute("target", "_blank");
-  link.href = video.src;
-  document.body.appendChild(link);
-  link.click();
-  link.remove();
-});
+function downloadRecording() {
+  const blob = new Blob(recordedChunks, { type: 'video/webm' });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'recording.webm';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+}
